27cda1871e7ff805b701366691faa9d355b3239b
Gustavo <gustavoooliveiradecarvalho@gmail.com>
2024-06-13 19:45:02 -0400

    Chore: done ex. 02 - js
    - use if to check is valid
    - use loop for N times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(str, nTimes) {
+    if (nTimes < 0) return 'ERROR';
 
+    let repeatedStr = '';
+    for (let i = 0; i < nTimes; i++){
+        repeatedStr += str;
+    }
+    return repeatedStr;
 };
 
 // Do not edit below this line
