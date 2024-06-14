38c9c06924d1d250a20321882458e33d338f012b
Gustavo <gustavoooliveiradecarvalho@gmail.com>
2024-06-14 11:53:50 -0400

    Chore: done ex. 11 - js
    - take soo long to do this
    - however, I did it by my self

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let groupedTitles = [];
+    books.forEach(book => {
+        groupedTitles.push(book.title);
+    });
+    return groupedTitles;
 };
 
 // Do not edit below this line
