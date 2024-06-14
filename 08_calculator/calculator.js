492f4991bbd0fa4878b68cfdeb496e4dd1ee45ad
Gustavo <gustavoooliveiradecarvalho@gmail.com>
2024-06-14 10:00:51 -0400

    Chore: done in smart way

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -7,27 +7,15 @@ const subtract = function(num1, num2) {
 };
 
 const sum = function(numsAry) {
-  let sum = 0;
-  numsAry.forEach( num => {
-    sum += num;
-  });
-  return sum;
+  return numsAry.reduce( (total, num) => total + num, 0);
 };
 
 const multiply = function(numsAry) {
-  let mult = 1;
-  numsAry.forEach( num => {
-    mult *= num;
-  });
-  return mult;
+  return numsAry.reduce((total, num) => total * num, 1);
 };
 
-const power = function(num, pow) {
-	let total = 1;
-  for (let i = 0; i < pow; i++){
-    total *= num;
-  }
-  return total;
+const power = function(num, power) {
+	return Math.pow(num, power)
 };
 
 const factorial = function(num) {
