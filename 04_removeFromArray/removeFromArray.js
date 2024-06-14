48e467ade0f0c28d514c5162e10c35e31e55d42a
Gustavo <gustavoooliveiradecarvalho@gmail.com>
2024-06-13 20:53:22 -0400

    Chore: done ex. 04 - js
    - see the solution for learn how use the rest param
    - most part done by myself

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(ary, ...targets) {
+    let newAry = [];
+    ary.forEach( item => {
+    if (!targets.includes(item))
+        newAry.push(item)
+    })
+    return newAry;
 };
 
 // Do not edit below this line
