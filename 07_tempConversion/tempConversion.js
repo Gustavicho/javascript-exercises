9e0fcd74bc232524e85c69dd4a32c5305a2eb9de
Gustavo <gustavoooliveiradecarvalho@gmail.com>
2024-06-13 23:49:19 -0400

    Chore: done ex. 07 - js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let cel = (temp - 32) / 1.8;
+  return Math.round(cel * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fah = temp * 1.8 + 32;
+  return Math.round(fah * 10) / 10;
 };
 
 // Do not edit below this line
