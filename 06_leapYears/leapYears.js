f07e7ceb5fd18b03c00cff21a2b5c5ea3ee91ab8
Gustavo <gustavoooliveiradecarvalho@gmail.com>
2024-06-13 23:35:22 -0400

    Chore: done ex. 06 - js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // leap year is divisible by 4 and can't be divisible by 100.
+    // However, IF divisible by 100 need be divisible by 400 to be a leap year
+    return year % 4 === 0 && year % 100 !== 0 || year % 100 === 0  && year % 400 === 0
 };
 
 // Do not edit below this line
