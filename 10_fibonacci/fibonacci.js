6d814df58af9ee2e0f2de5e59927992117a1dbbf
Gustavo <gustavoooliveiradecarvalho@gmail.com>
2024-06-14 11:09:43 -0400

    Chore: done ex. 10 - js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    let count = typeof index === 'number' ? index : parseInt(index);
 
+    if (count < 0) return 'OOPS';
+    if (count === 0) return 0;
+
+    let fib = [1, 1]
+    for (let i = 2; i < count; i++){
+        fib[i]= fib[i-1] + fib[i-2];
+    }
+    return fib[count-1]
 };
 
 // Do not edit below this line
