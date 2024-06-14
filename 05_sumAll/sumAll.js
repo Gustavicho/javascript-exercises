d51f703e7a01aedfed262e2da16b764ee3493d5a
Gustavo <gustavoooliveiradecarvalho@gmail.com>
2024-06-13 23:19:09 -0400

    Chore: done ex. 05 - js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return 'ERROR';
+    if (min < 0 || max < 0) return 'ERROR';
+    if (min > max){
+        const TEMP = min;
+        min = max;
+        max = TEMP;
+    }    
+    
+    let sumTotal = 0
+    for (; min <= max; min++) {
+        sumTotal += min
+    }
+    return sumTotal
 };
 
 // Do not edit below this line
