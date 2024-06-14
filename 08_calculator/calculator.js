1bc2acc25e5ac47ec8962cdcf62adc794a1ce94b
Gustavo <gustavoooliveiradecarvalho@gmail.com>
2024-06-14 09:56:21 -0400

    Chore: done ex. 08 - js
    - done in my way

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numsAry) {
+  let sum = 0;
+  numsAry.forEach( num => {
+    sum += num;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numsAry) {
+  let mult = 1;
+  numsAry.forEach( num => {
+    mult *= num;
+  });
+  return mult;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+	let total = 1;
+  for (let i = 0; i < pow; i++){
+    total *= num;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let total = 1;
+  for (let i = num; i > 0; i--){
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
