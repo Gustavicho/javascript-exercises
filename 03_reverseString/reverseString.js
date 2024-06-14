c9d1069e6dbe9592ad22074de9f02f186662835f
Gustavo <gustavoooliveiradecarvalho@gmail.com>
2024-06-13 20:02:26 -0400

    Chore: done ex. 03 - js
    - diferents ways to do

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    // turn in ary, reverse position, join in a str
+    return str.split('').reverse().join('');
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+/*  MY WAY
+    let reversedStr = '';
+    for (let i = str.length; i >= 0; i--){
+        reversedStr += str.charAt(i);
+    }
+    return reversedStr;
+*/
